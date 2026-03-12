2d221d9c1c727366160f21bfa71ca41003d38014
leo <lelelouisedu77@gmail.com>
2026-03-12 14:24:39 +0100

    widget.js

--- embed/widget.js
+++ embed/widget.js
@@ -8,17 +8,15 @@
   button.style.right = "20px";
   button.style.background = "black";
   button.style.color = "white";
-  button.style.padding = "10px";
+  button.style.padding = "10px 15px";
   button.style.borderRadius = "20px";
   button.style.cursor = "pointer";
+  button.style.zIndex = "9999";
 
   button.onclick = () => {
     alert("Chat ouvert !");
   };
 
   document.body.appendChild(button);
-
-})();
-  document.body.appendChild(chatBox);
 
 })();
