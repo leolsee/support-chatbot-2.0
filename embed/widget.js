8ffdd13bb3e476f6f62c534cf30f59ec5eeb96ab
leo <lelelouisedu77@gmail.com>
2026-03-12 14:06:32 +0100

    widget.js

---
+++ embed/widget.js
@@ -0,0 +1,16 @@
+(function () {
+
+  const chat = document.createElement("div");
+  chat.innerHTML = "💬 Chat Support";
+
+  chat.style.position = "fixed";
+  chat.style.bottom = "20px";
+  chat.style.right = "20px";
+  chat.style.background = "black";
+  chat.style.color = "white";
+  chat.style.padding = "10px";
+  chat.style.borderRadius = "10px";
+
+  document.body.appendChild(chat);
+
+})();
