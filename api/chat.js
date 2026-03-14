19311cb9bc846387a36b811d97ad006e983bf88b
leo <lelelouisedu77@gmail.com>
2026-03-14 22:36:11 +0100

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     }
 
     // sauvegarder message user
-    await supabase.from("messages").insert({
+    await supabase.from("messages").insert([
       conversation_id: 1,
       expediteur: "user",
       message: message
