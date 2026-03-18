d523e012032cd3926ccf241471dc7d036d9a61da
leo <lelelouisedu77@gmail.com>
2026-03-18 17:30:10 +0100

    index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
-if (req.method === "POST") {
-  return res.json({
-    reply: "OK TEST BACKEND ✅",
-  });
+export default async function handler(req, res) {
+  try {
+    const { message } = req.body;
+
+    console.log("MESSAGE:", message);
+
+    return res.status(200).json({
+      reply: "Tu as dit : " + message
+    });
+
+  } catch (err) {
+    console.error("ERROR:", err);
+
+    return res.status(500).json({
+      reply: "Erreur serveur"
+    });
+  }
 }
