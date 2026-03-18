0d5349f34fd9b0aad4b5f90b680826db6f21ebb8
leo <lelelouisedu77@gmail.com>
2026-03-18 17:44:36 +0100

    index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,5 @@
-export default async function handler(req, res) {
-  try {
-    const { message } = req.body;
-
-    console.log("MESSAGE:", message);
-
-    return res.status(200).json({
-      reply: "Tu as dit : " + message
-    });
-
-  } catch (err) {
-    console.error("ERROR:", err);
-
-    return res.status(500).json({
-      reply: "Erreur serveur"
-    });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    reply: "Test OK"
+  });
 }
